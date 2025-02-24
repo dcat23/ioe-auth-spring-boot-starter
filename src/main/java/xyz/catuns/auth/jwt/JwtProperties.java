70ebd87c924e17a8a9d02addd1c88e9ff75b0a8e
Devin Catuns <devincatuns1@gmail.com>
2025-02-24 17:31:50 -0600

    fix: set io.jsonwebtoken deps as optional.

--- src/main/java/xyz/catuns/auth/jwt/JwtProperties.java
+++ src/main/java/xyz/catuns/auth/jwt/JwtProperties.java
@@ -8,11 +8,10 @@ public record JwtProperties(
 
         @DefaultValue("36000000") // 10 hours
         Long expiration,
-        @DefaultValue("https://catuns.xyz")
+        @DefaultValue("https://auth.catuns.xyz/issuer-not-set")
         String issuer,
-        @DefaultValue("DO_NOT_USE_THIS_RANDOM_SECRET")
+        @DefaultValue("DO_NOT_USE_THIS_RANDOM_SECRET") // todo: should default value be set at all?
         String secret
-
 ) {
 
 }
